51a5dc3a3886d44d76fcde7dcb7a341afd898dde
Sriram Ravula <sriram.ravula@utexas.edu>
2019-12-09 21:47:40 -0600

    Updating newest

--- Demo.py
+++ Demo.py
@@ -9,6 +9,7 @@ import time
 import DQN 
 import matplotlib.pyplot as plt
 import one_hot_to_midi as oh
+import sys
 
 #Run a demo using the given parameters
 def run_demo(num_samples, model):
@@ -18,23 +19,23 @@ def run_demo(num_samples, model):
 		weights = "NOTE_CNN_WEIGHTS_400.pt"
 		samples = nrnn.generate_samples_NoteCNN(weights, 32, 10, num_samples)
 
-	elif model == "0.01"
+	elif model == "0.01":
 		weights = "Q_400-500000.pt"
 		samples = DQN.generate_sample(weights, 32, 10, num_samples)
 
-	elif model == "0.05"
+	elif model == "0.05":
 		weights = "Q_500-100000.pt"
 		samples = DQN.generate_sample(weights, 32, 10, num_samples)
 
-	elif model == "0.1"
+	elif model == "0.1":
 		weights = "Q-500000.pt"
 		samples = DQN.generate_sample(weights, 32, 10, num_samples)
 
-	elif model == "0.3"
+	elif model == "0.3":
 		weights = "Q_300-500000.pt"
 		samples = DQN.generate_sample(weights, 32, 10, num_samples)
 
-	elif model == "0.5"
+	elif model == "0.5":
 		weights = "Q_200-500000.pt"
 		samples = DQN.generate_sample(weights, 32, 10, num_samples)
 	else:
@@ -47,6 +48,6 @@ def run_demo(num_samples, model):
 	return None
 
 model = sys.argv[1]
-num_samples = sys.argv[2]
+num_samples = int(sys.argv[2])
 
 run_demo(num_samples, model)
